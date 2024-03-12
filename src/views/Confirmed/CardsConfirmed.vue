a6516a210b167d7a0377c5852d849180c9db0eea
hamza <hamdihamza440@gmail.com>
2024-03-12 18:35:42 +0100

    Revert "create function confirmation rate and order confirmed for cards confirmed"
    This reverts commit df0d5695c5fb84fb6ac5c13e84a342f81cf90324.

--- src/views/Confirmed/CardsConfirmed.vue
+++ src/views/Confirmed/CardsConfirmed.vue
@@ -1,79 +1,44 @@
 <template>
     <div class="carts-confirmed">
-        <CardGlobal :carts="allCards" />
+        <CardGlobal :carts="carts" />
     </div>
 </template>
 
 <script>
-    import {
-        mapState
-    } from 'vuex'
     import CardGlobal from '@/components/CardGlobal.vue'
     export default {
         name: 'CardsConfirmed',
         components: {
             CardGlobal,
         },
-
-        computed: {
-            ...mapState('NewOrders', {
-                StoreNewOrders: state => state
-            }),
-            ...mapState('OrderConfirmed', {
-                storeConfirmed: state => state
-            }),
-
-            allCards() {
-                let carts = [
+        data() {
+            return {
+                
+                carts: [
 
                     {
-                        classicon: 'Order-Confirmed',
-                        icon: 'bx bx-package',
-                        title: 'Order Confirmed',
-                        number: `${this.NumbersOrdersConfirmed}/${this.NumbersNewOrders}`,
-                        pourcentage: 3.20,
-                        duration: 'Today'
-
-                    },
+                    classicon: 'Order-Confirmed',
+                    icon: 'bx bx-package',
+                    title: 'Order Confirmed',
+                    number: "120 / 234",
+                    pourcentage: 3.20,
+                    duration:'Today'
+                   
+                },
                     {
 
-                        classicon: 'confirmation-rate',
-                        icon: 'bx bx-chart',
-                        title: 'Confirmation Rate',
-                        number: `${this.ConfirmationRate}%`,
-                        pourcentage: 3.20,
-                        duration: 'Today'
-                    }
-                ]
+                    classicon: 'confirmation-rate',
+                    icon: 'bx bx-chart',
+                    title: 'Confirmation Rate',
+                    number: '57%',
+                    pourcentage: 3.20,
+                    duration:'Today'
+                }
+            ]
 
-                return carts
-            },
-
-            NumbersNewOrders() {
-                let numbOrders = Object.values(this.StoreNewOrders).reduce((accu, table) => {
-                    return accu + table.length
-                }, 0)
-                return numbOrders
-            },
-
-            NumbersOrdersConfirmed() {
-                let numbOrders = Object.values(this.storeConfirmed).reduce((accu, table) => {
-                    return accu + table.length
-                }, 0)
-                return numbOrders
-            },
-
-            ConfirmationRate(){
-                let pourcentage= Math.floor(this.NumbersOrdersConfirmed/this.NumbersNewOrders * 100)
-                return pourcentage
             }
-
-        },
-
-        mounted(){
-            console.log(this.NumbersNewOrders)
-            console.log(this.NumbersOrdersConfirmed)
         }
 
     }
 </script>
+
