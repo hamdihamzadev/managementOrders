27917ffa4044995e03f7f689e744787b186dc85f
hamza <hamdihamza440@gmail.com>
2024-03-24 12:18:19 +0100

    add computed order/sales/delivery-rate in page deliverd

--- src/views/Deliverd/CardsDeliverd.vue
+++ src/views/Deliverd/CardsDeliverd.vue
@@ -1,34 +1,37 @@
 <template>
     <div class="mb-4">
         <CardGlobal 
-        :carts="AllCarts"
+        :carts="cards"
         />
     </div>
 </template>
 
 <script>
+
 import CardGlobal from '@/components/CardGlobal.vue'
+import { mapState } from 'vuex'
 export default{
     name:'CardsDeliverd',
     components: {
         CardGlobal
         },
-        data() {
-            return {
-                AllCarts: [
+             
+        computed:{
+            cards(){
+                let AllCards=[
                     {
                         classicon: 'Shipped',
                         icon: 'bx bx-trip',
                         title: 'Order Delivred',
-                        number: '102/320',
+                        number: `${this.NumberOrdersDelivred} / ${this.NumberOrdersShipped} `,
                         pourcentage: 5.43,
                         duration:'Days'
                     },
                     {
                         classicon: 'income',
                         icon: 'bx bx-money',
-                        title: 'Income',
-                        number: 1634,
+                        title: 'Sales',
+                        number: this.sales,
                         dollar: '$',
                         pourcentage: 4.87
                     },
@@ -37,13 +40,45 @@ export default{
                         classicon: 'delivery-rate',
                         icon: 'bx bx-money',
                         title: 'Delivery Rate',
-                        number: '3.23 %',
+                        number: `${this.deliveryRate} %`,
                         pourcentage: 2.43,
                         duration:'Days'
                     },
 
                 ]
+                return AllCards
+            },
+
+            ...mapState('allOrder', {
+                AllOrdersShipped: state => state.shipped
+            }),
+            ...mapState('allOrder', {
+                AllOrdersDelivered: state => state.Delivered
+            }),
+
+            NumberOrdersShipped(){
+                let numbers=this.AllOrdersShipped.length
+                return numbers
+            },
+           
+            NumberOrdersDelivred(){
+                let numbers=this.AllOrdersDelivered.length
+                return numbers
+            },
+
+            sales(){
+
+                let totalSales=this.AllOrdersDelivered.reduce((accu,order)=>{
+                    return accu+order.total
+                },0)
+                return totalSales
+            },
+
+            deliveryRate(){
+                let percentage= Math.floor((this.NumberOrdersDelivred/this.NumberOrdersShipped)*100)
+                return percentage
             }
-        }    
+    
+        }
 }
 </script>
