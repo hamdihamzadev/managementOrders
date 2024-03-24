75694f6c92a2d982cff1ece63d7b699e152b0c08
hamza <hamdihamza440@gmail.com>
2024-03-24 12:42:52 +0100

    add compted calc cancelled rate and order cancelled

--- src/views/Canceld/CardsCaanceld.vue
+++ src/views/Canceld/CardsCaanceld.vue
@@ -1,44 +1,69 @@
 <template>
     <div class="carts-confirmed">
-        <CardGlobal :carts="carts" />
+        <CardGlobal :carts="cards" />
     </div>
 </template>
 
 <script>
+
     import CardGlobal from '@/components/CardGlobal.vue'
+import { mapState } from 'vuex'
     export default {
         name: 'CartsCanceld',
         components: {
             CardGlobal,
         },
-        data() {
-            return {
-                
-                carts: [
+  
+        computed: {
+            cards() {
+                let allCards = [
 
                     {
-                    classicon: 'Order-Confirmed',
-                    icon: 'bx bx-package',
-                    title: 'Order Canceld',
-                    number: "23 /138",
-                    pourcentage: 1.20,
-                    duration:'Today'
-                   
-                },
+                        classicon: 'Order-Confirmed',
+                        icon: 'bx bx-package',
+                        title: 'Order Canceld',
+                        number: `${this.Orderscancelled} / ${this.newOrder}`,
+                        pourcentage: 1.20,
+                        duration: 'Today'
+
+                    },
                     {
 
-                    classicon: 'confirmation-rate',
-                    icon: 'bx bx-chart',
-                    title: 'Canceld Rate',
-                    number: '3%',
-                    pourcentage: 1.20,
-                    duration:'Today'
-                }
-            ]
+                        classicon: 'confirmation-rate',
+                        icon: 'bx bx-chart',
+                        title: 'cancelled Rate',
+                        number: `${this.cancelledRate} %`,
+                        pourcentage: 1.20,
+                        duration: 'Today'
+                    }
+                ]
+                return allCards
+            },
 
+            ...mapState('allOrder', {
+                AllNewOrders: state => state.new
+            }),
+
+            ...mapState('allOrder', {
+                AllOrderscancelled: state => state.cancelled
+            }),
+
+            newOrder(){
+                let numbersOrders=this.AllNewOrders.length
+                return numbersOrders
+            },
+
+            Orderscancelled(){
+                let numbersOrders=this.AllOrderscancelled.length
+                return numbersOrders
+            },
+
+            cancelledRate(){
+                let percentage= Math.floor((this.Orderscancelled/this.newOrder)*100)
+                return percentage
             }
+
         }
 
     }
 </script>
-
